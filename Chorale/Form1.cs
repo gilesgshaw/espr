e2f5060ff8330f9cee583d9d22291e4f6da42769
Giles Shaw <gilesgshaw@gmail.com>
2021-07-07 23:38:47 +0100

    minor

--- Chorale/Form1.cs
+++ Chorale/Form1.cs
@@ -16,5 +16,10 @@ namespace Chorale
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mus.notation.test();
+        }
     }
 }
