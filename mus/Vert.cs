b997401dfa2e7200c390dafbd970c5973f1490f7
Giles Shaw <gilesgshaw@gmail.com>
2021-07-12 17:02:31 +0100

    addition

---
+++ mus/Vert.cs
@@ -0,0 +1,21 @@
+﻿namespace mus
+{
+    public static partial class notation
+    {
+
+        public class Vert
+        {
+            public Chord Chord { get; }
+            public VoicingC Voicing { get; }
+            public VoicingC Absolute { get; }
+
+            public Vert(Chord chord, VoicingC voicing)
+            {
+                Chord = chord;
+                Voicing = voicing;
+                Absolute = Chord.Root + voicing;
+            }
+        }
+
+    }
+}
