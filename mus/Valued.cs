ba5839212c413404ff2ea36f92ea890182077a6d
Giles Shaw <gilesgshaw@gmail.com>
2021-07-12 17:47:45 +0100

    first step

---
+++ mus/Valued.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace mus
+{
+    public static partial class notation
+    {
+
+        class Valuer<T> : IComparer<T> where T : Valued
+        {
+            public int Compare(T x, T y)
+            {
+                return Comparer<double>.Default.Compare(x.Penalty, y.Penalty);
+            }
+
+            private Valuer() { }
+
+            public readonly static Valuer<T> instance = new Valuer<T>();
+        }
+
+        public abstract class Valued
+        {
+
+            public virtual double Penalty { get; }
+
+            protected Valued(Valued[] children)
+            {
+                Penalty = children.Aggregate(0D, (x, y) => x + y.Penalty);
+            }
+        }
+
+    }
+}
