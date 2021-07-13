ef794b601b42d3a3352b5620c9f1506bdfc8e1be
Giles Shaw <gilesgshaw@gmail.com>
2021-07-13 20:11:00 +0100

    valuation improve

--- mus/Passage.cs
+++ mus/Passage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Math;
 
 namespace mus
 {
@@ -21,11 +22,27 @@ namespace mus
                 get
                 {
                     var tr = base.Penalty;
+                    for (int i = 1; i < Chords.Length; i++)
+                    {
+                        var RootChange = Verts[i].Chord.Root.ResidueSemis - Verts[i - 1].Chord.Root.ResidueSemis;
+                        var SChange = Verts[i].Voicing.S.Semis - Verts[i - 1].Voicing.S.Semis;
+                        var AChange = Verts[i].Voicing.A.Semis - Verts[i - 1].Voicing.A.Semis;
+                        var TChange = Verts[i].Voicing.T.Semis - Verts[i - 1].Voicing.T.Semis;
+                        var BChange = Verts[i].Voicing.B.Semis - Verts[i - 1].Voicing.B.Semis;
+                        tr += Abs(RootChange+SChange);
+                        tr += Abs(RootChange+AChange);
+                        tr += Abs(RootChange+TChange);
+                        tr += Abs(RootChange+BChange);
+                        if (ReferenceEquals(Chords[i], Chords[i - 1]) && Verts[i].Voicing.B.Residue == Verts[i - 1].Voicing.B.Residue)
+                        {
+                            tr += 60;
+                        }
+                    }
                     return tr;
                 }
             }
 
-            public Passage(IntervalS tonic, Vert[] verts) : base()
+            public Passage(IntervalS tonic, Vert[] verts) : base(verts)
             {
                 Tonic = tonic;
                 Chords = Array.ConvertAll(verts, (x) => x.Chord);
