5a75ac8bcf43b27495e3820813cc74630e1f6f99
Giles Shaw <gilesgshaw@gmail.com>
2021-07-13 15:53:49 +0100

    cosmetic

--- Chorale/Form1.Designer.cs
+++ Chorale/Form1.Designer.cs
@@ -35,19 +35,20 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(33, 194);
+            this.button1.Location = new System.Drawing.Point(68, 124);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 30);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Get Cadence";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(154, 23);
+            this.pictureBox1.Location = new System.Drawing.Point(197, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(340, 394);
+            this.pictureBox1.Size = new System.Drawing.Size(276, 267);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -55,11 +56,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(511, 291);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Chorale";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
