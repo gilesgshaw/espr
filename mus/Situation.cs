2a5f7e285e0a2755ca8b2f05bbc6a8aec3cf2383
Giles Shaw <gilesgshaw@gmail.com>
2021-07-26 12:19:12 +0100

    rearrange

--- mus/Situation.cs
+++ mus/Situation.cs
@@ -10,6 +10,103 @@ namespace mus
         public class Situation : IEquatable<Situation>
         {
 
+
+            #region Static
+
+            //not yet implemented tolerence
+            public static IEnumerable<Passage> GetSingletons(Situation situation, double[] tolerence)
+            {
+                return Array.ConvertAll(situation.Context.Bank[situation.Sop[0]], (x) => new Passage(situation.Context.Tonic, new Vert[] { x }, null, null));
+            }
+
+            //not yet implemented tolerence
+            public static IEnumerable<Passage> GetPairs(Situation situation, double[] tolerence)
+            {
+                if (situation.Displacement == 0)
+                {
+                    foreach (var final in situation.Context.Bank[situation.Sop[1]].Where((x) => x.Chord.Root.ResidueNumber == 0 && x.Voicing.B.ResidueNumber == 0))
+                    {
+                        foreach (var pp in situation.Context.Bank[situation.Sop[0]].Where((x) => x.Voicing.B.ResidueNumber == 0))
+                        {
+                            switch (pp.Chord.Root.ResidueNumber)
+                            {
+                                case 4:
+                                    yield return new Passage(
+                                        situation.Context.Tonic,
+                                        new Vert[] { pp, final },
+                                        new Passage(situation.Context.Tonic, new Vert[] { pp }, null, null),
+                                        new Passage(situation.Context.Tonic, new Vert[] { final }, null, null)
+                                        );
+                                    break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var final in situation.Context.Bank[situation.Sop[1]])
+                    {
+                        foreach (var pp in situation.Context.Bank[situation.Sop[0]])
+                        {
+                            yield return new Passage(
+                                situation.Context.Tonic,
+                                new Vert[] { pp, final },
+                                new Passage(situation.Context.Tonic, new Vert[] { pp }, null, null),
+                                new Passage(situation.Context.Tonic, new Vert[] { final }, null, null)
+                                );
+                        }
+                    }
+                }
+            }
+
+            //at least 1
+            public static Dictionary<Situation, List<Passage>> Cache = new Dictionary<Situation, List<Passage>>();
+
+            //at least 1
+            public static IEnumerable<Passage> GetExterenal(Situation situation, double[] tolerence)
+            {
+                if (situation.Sop.Length == 1)
+                {
+                    //exactly 1
+                    if (!Cache.ContainsKey(situation)) Cache.Add(situation, new List<Passage>(GetSingletons(situation, tolerence)));
+                }
+                else if (situation.Sop.Length == 2)
+                {
+                    //exactly 2
+                    if (!Cache.ContainsKey(situation)) Cache.Add(situation, new List<Passage>(GetPairs(situation, tolerence)));
+                }
+                else
+                {
+                    //at least 3
+                    AddExterenal(situation, tolerence);
+                }
+                foreach (var item in Cache[situation])
+                {
+                    yield return item;
+                }
+            }
+
+            //at least 3
+            //tolerence dosen't matter if already present.
+            public static void AddExterenal(Situation situation, double[] tolerence)
+            {
+                if (Cache.ContainsKey(situation)) return;
+
+                var list = new List<Passage>();
+                Cache.Add(situation, list);
+                foreach (var l in GetExterenal(situation.Left, tolerence))
+                {
+                    foreach (var r in GetExterenal(situation.Right, tolerence).Where((x) => x.Left == l.Right))
+                    {
+                        var obj = new Passage(l.Tonic, l.Verts.Concat(new Vert[] { r.Verts.Last() }).ToArray(), l, r);
+                        if (obj.Penalty <= tolerence[obj.Verts.Length]) list.Add(obj);
+                    }
+                }
+            }
+
+            #endregion
+
+
             public Context Context;
             public int[] Sop; //length at least 1
             public int Displacement;
