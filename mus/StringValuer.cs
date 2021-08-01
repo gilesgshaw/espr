050c186e0d8ababa14b6e43d383bd6d7428c91f8
Giles Shaw <gilesgshaw@gmail.com>
2021-08-02 00:51:02 +0100

    add - 'string valuer' class

---
+++ mus/StringValuer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace mus
+{
+
+    public abstract class StringValuer<TProblem, TSolution> : StringSolver<TProblem, TSolution> where TSolution : notation.Valuable
+    {
+        protected override IEnumerable<(TSolution, T)> Refine<T>(TProblem problem, (TSolution, T)[] solutions)
+        {
+            var permissableSolutions = new List<(TSolution, T)>();
+            var tolerence = Tolerence(problem);
+            foreach (var solution in solutions)
+            {
+                if (solution.Item1.Penalty <= tolerence) permissableSolutions.Add(solution);
+            }
+            permissableSolutions.Sort(Compare.With<(TSolution, T)>((x, y) => x.Item1.Penalty.CompareTo(y.Item1.Penalty)));
+            return permissableSolutions.Take(Capacity(problem));
+        }
+
+        protected abstract int Capacity(TProblem problem);
+        protected abstract double Tolerence(TProblem problem);
+
+        protected StringValuer(IEqualityComparer<TProblem> comparer) : base(comparer) { }
+        protected StringValuer() : base() { }
+    }
+
+}
