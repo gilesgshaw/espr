0cddef439f10ffa287d595bf64fd9513d9f94350
Giles Shaw <gilesgshaw@gmail.com>
2021-08-01 19:07:08 +0100

    add - utilities for IComparer and IEqComparer

--- mus/Utility.cs
+++ mus/Utility.cs
@@ -9,6 +9,60 @@ using System.Text.Json.Serialization;
 
 namespace mus
 {
+
+    public static class Equate
+    {
+        public static xEqualityComparer<T> When<T>(Func<T, T, bool> equals, Func<T, int> getHashCode)
+        {
+            return new xEqualityComparer<T>(equals, getHashCode);
+        }
+    }
+
+    public class xEqualityComparer<T> : IEqualityComparer<T>
+    {
+        public xEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode)
+        {
+            iEquals = equals;
+            iGetHashCode = getHashCode;
+        }
+
+        private Func<T, T, bool> iEquals;
+        private Func<T, int> iGetHashCode;
+
+        public bool Equals(T x, T y)
+        {
+            return iEquals(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return iGetHashCode(obj);
+        }
+    }
+
+    public static class Compare
+    {
+        public static xComparer<T> With<T>(Func<T, T, int> compare)
+        {
+            return new xComparer<T>(compare);
+        }
+    }
+
+    public class xComparer<T> : IComparer<T>
+    {
+        public xComparer(Func<T, T, int> compare)
+        {
+            iCompare = compare;
+        }
+
+        private Func<T, T, int> iCompare;
+
+        public int Compare(T x, T y)
+        {
+            return iCompare(x, y);
+        }
+    }
+
     public static class Ut
     {
 
@@ -30,4 +84,5 @@ namespace mus
         }
 
     }
+
 }
