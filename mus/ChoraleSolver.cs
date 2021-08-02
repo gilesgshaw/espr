e421c9e6b23483a392ec8926d93d9027b7761456
Giles Shaw <gilesgshaw@gmail.com>
2021-08-02 14:20:42 +0100

    add - ChoraleSolver specialising StringValuer

---
+++ mus/ChoraleSolver.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static mus.notation;
+using mus;
+
+namespace Chorale
+{
+
+    public class ChoraleSolver : StringValuer<PassageSt, Passage>
+    {
+        protected override int Capacity(PassageSt problem) => caps[problem.Sop.Length];
+        protected override double Tolerence(PassageSt problem) => tols[problem.Sop.Length];
+
+        private readonly int[] caps;
+        private readonly double[] tols;
+
+        public ChoraleSolver(int[] caps, double[] tols) : base(Equate.When<PassageSt>(
+            (x, y) => ReferenceEquals(x, y),
+            (x) => x == null ? 0 : x.GetHashCode()))
+        {
+            this.caps = caps;
+            this.tols = tols;
+        }
+
+
+        protected override IEnumerable<(Passage, T)> Refine<T>(PassageSt situation, (Passage, T)[] solutions)
+        {
+            if (situation.Sop.Length == 1 && situation.Initial)                // opening chord (i.e. I(a))
+            {
+                return base.Refine(situation, solutions.Where((x) =>
+                {
+                    return x.Item1.Chords[0].Root.ResidueNumber == 0 && x.Item1.Verts[0].Voicing.B.ResidueNumber == 0;
+                }
+                ).ToArray());
+            }
+            else if (situation.Sop.Length == 2 && situation.Displacement == 0) // cadence
+            {
+                return base.Refine(situation, solutions.Where((x) =>
+                {
+                    return  //   perfect V-I
+                    (x.Item1.Chords[0].Root.ResidueNumber == 4 && x.Item1.Verts[0].Voicing.B.ResidueNumber == 0 &&
+                    x.Item1.Chords[1].Root.ResidueNumber == 0 && x.Item1.Verts[1].Voicing.B.ResidueNumber == 0)
+                    ||      // imperfect ?-V
+                    (!x.Item1.Verts[1].Chord.Variety.PQ7.HasValue &&
+                    x.Item1.Chords[1].Root.ResidueNumber == 4 && x.Item1.Verts[1].Voicing.B.ResidueNumber == 0);
+                }
+                ).ToArray());
+            }
+            else                                                               // anything else
+            {
+                return base.Refine(situation, solutions);
+            }
+            //could also check if this is a final chord (i.e. should be I or V)
+        }
+
+
+        protected override Passage Combine(Passage l, Passage r) => new Passage(l.Tonic, l.Verts.Concat(new Vert[] { r.Verts.Last() }).ToArray(), l, r);
+        protected override PassageSt Left(PassageSt parent) => parent.Left;
+        protected override PassageSt Right(PassageSt parent) => parent.Right;
+        protected override IEnumerable<Passage> SolveSingleton(PassageSt problem) =>
+            problem.Context.Bank[problem.Sop[0]].Select((x) => new Passage(problem.Context.Tonic, new[] { x }, null, null));
+    }
+
+}
