6c6e16f4a4b355e9ab74b63781c5b3b668c21b12
Giles Shaw <gilesgshaw@gmail.com>
2021-08-04 15:47:35 +0100

    setup fairly good rules (Phrase.cs)

--- mus/Phrase.cs
+++ mus/Phrase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using static System.Math;
 using static mus.Ut;
 
 namespace mus
@@ -106,6 +107,202 @@ namespace mus
 
                 return true;
             }
+
+
+            public override double IntrinsicPenalty
+            {
+                get
+                {
+                    var tr = base.IntrinsicPenalty;
+
+                    // setup
+                    var PitchMx = Array.ConvertAll(Pitches.ToArray(),
+                        (x) => new Pitch[] {
+                            x.S,
+                            x.A,
+                            x.T,
+                            x.B
+                        });
+                    var PitchMxR = Array.ConvertAll(Pitches.ToArray(),
+                        (x) => new IntervalS[] {
+                            x.S.FromC0.Residue,
+                            x.A.FromC0.Residue,
+                            x.T.FromC0.Residue,
+                            x.B.FromC0.Residue
+                        });
+                    var TrsMx =
+                        Enumerable.Zip(PitchMx, PitchMx.Skip(1),
+                        (x, y) =>
+                        {
+                            return Enumerable.Zip(x, y,
+                                (a, b) => b - a)
+                            .ToArray();
+                        }
+                        ).ToArray();
+                    var TrsMxR =
+                        Enumerable.Zip(PitchMx, PitchMx.Skip(1),
+                        (x, y) =>
+                        {
+                            return Enumerable.Zip(x, y,
+                                (a, b) => (b - a).Residue)
+                            .ToArray();
+                        }
+                        ).ToArray();
+                    var TrsAr =
+                        Enumerable.Zip(Pitches, Pitches.Skip(1),
+                        (x, y) => (
+                            S: y.S - x.S,
+                            A: y.A - x.A,
+                            T: y.T - x.T,
+                            B: y.B - x.B
+                        )).ToArray();
+
+
+                    if (Length == 1) // Length 1
+                    {
+
+                        //Bad chords
+                        if (LTonic[0] == RTonic[0])
+                        {
+                            if (LChords[0].Root.ResidueNumber == 5 && LVerts[0].Voicing.B.ResidueNumber == 2) tr += 35;
+                            if (LChords[0].Root.ResidueNumber == 1 && LVerts[0].Voicing.B.ResidueNumber == 0) tr += 15;
+                        }
+                        else
+                        {
+                            if (LChords[0].Root.ResidueNumber == 5 && LVerts[0].Voicing.B.ResidueNumber == 2) tr += 45;
+                            if (LChords[0].Root.ResidueNumber == 1 && LVerts[0].Voicing.B.ResidueNumber == 0) tr += 25;
+                            if (RChords[0].Root.ResidueNumber == 5 && RVerts[0].Voicing.B.ResidueNumber == 2) tr += 45;
+                            if (RChords[0].Root.ResidueNumber == 1 && RVerts[0].Voicing.B.ResidueNumber == 0) tr += 25;
+                        }
+
+                        //Doubling 5th or root in chord 7
+                        if (LChords[0].Root.ResidueNumber == 6 || RChords[0].Root.ResidueNumber == 6)
+                        {
+                            int[] profile = new int[7];
+                            profile[LVerts[0].Voicing.B.ResidueNumber]++;
+                            profile[LVerts[0].Voicing.T.ResidueNumber]++;
+                            profile[LVerts[0].Voicing.A.ResidueNumber]++;
+                            profile[LVerts[0].Voicing.S.ResidueNumber]++;
+                            if (profile[4] >= 2) tr += 60;
+                            if (profile[0] >= 2) tr += 60;
+                        }
+
+                    }
+
+
+                    if (Length == 2) // Length 2
+                    {
+
+                        //Voice leading
+                        tr += Abs(TrsAr[0].A.Semis) + 2.5 * Max(0, Abs(TrsAr[0].A.Semis) - 3);
+                        tr += Abs(TrsAr[0].T.Semis) + 2.5 * Max(0, Abs(TrsAr[0].T.Semis) - 3);
+                        tr += 0.5 * Abs(TrsAr[0].B.Semis) + (1.8 * Max(0, Abs(TrsAr[0].B.Semis) - 4.5)
+                            * (1.5 * LVerts[1].Voicing.B.ResidueNumber + 1.5 * LVerts[0].Voicing.B.ResidueNumber + 1));
+                        double numStatic = 0;
+                        if (TrsAr[0].S.Semis == 0) numStatic += 1; //This should be left in!
+                        if (TrsAr[0].A.Semis == 0) numStatic += 1;
+                        if (TrsAr[0].T.Semis == 0) numStatic += 1.5;
+                        if (TrsAr[0].B.Semis == 0) numStatic += 2.5;
+                        tr += numStatic * numStatic * 3;
+
+                        //Arkward intervals
+                        if (TrsAr[0].S.Quality > 0 || TrsAr[0].S.Quality < -1 || (TrsAr[0].S.Quality < 0 && Degree.IsConsonant(TrsAr[0].S.ResidueNumber))) tr += 18;
+                        if (TrsAr[0].A.Quality > 0 || TrsAr[0].A.Quality < -1 || (TrsAr[0].A.Quality < 0 && Degree.IsConsonant(TrsAr[0].A.ResidueNumber))) tr += 18;
+                        if (TrsAr[0].T.Quality > 0 || TrsAr[0].T.Quality < -1 || (TrsAr[0].T.Quality < 0 && Degree.IsConsonant(TrsAr[0].T.ResidueNumber))) tr += 18;
+                        if (TrsAr[0].B.Quality > 0 || TrsAr[0].B.Quality < -1 || (TrsAr[0].B.Quality < 0 && Degree.IsConsonant(TrsAr[0].B.ResidueNumber))) tr += 18;
+
+                        //Resolving 7th
+                        //Can it resolve several chords later?
+                        if (LVerts[0].Voicing.S.ResidueNumber == 6 && TrsAr[0].S.Number != -1) tr += 70;
+                        if (LVerts[0].Voicing.A.ResidueNumber == 6 && TrsAr[0].A.Number != -1) tr += 70;
+                        if (LVerts[0].Voicing.T.ResidueNumber == 6 && TrsAr[0].T.Number != -1) tr += 70;
+                        if (LVerts[0].Voicing.B.ResidueNumber == 6 && TrsAr[0].B.Number != -1) tr += 70;
+
+                        //Preparing 7th
+                        //Should a 7th be prepared *exactly* in the previous chord? probably
+                        if (LVerts[1].Voicing.S.ResidueNumber == 6 && Abs(TrsAr[0].S.Number) >= 2) tr += 70;
+                        if (LVerts[1].Voicing.A.ResidueNumber == 6 && Abs(TrsAr[0].A.Number) >= 2) tr += 70;
+                        if (LVerts[1].Voicing.T.ResidueNumber == 6 && Abs(TrsAr[0].T.Number) >= 2) tr += 70;
+                        if (LVerts[1].Voicing.B.ResidueNumber == 6 && Abs(TrsAr[0].B.Number) >= 2) tr += 70;
+                        if (LVerts[1].Voicing.S.ResidueNumber == 6 && Abs(TrsAr[0].S.Number) == 1) tr += 35;
+                        if (LVerts[1].Voicing.A.ResidueNumber == 6 && Abs(TrsAr[0].A.Number) == 1) tr += 35;
+                        if (LVerts[1].Voicing.T.ResidueNumber == 6 && Abs(TrsAr[0].T.Number) == 1) tr += 35;
+                        if (LVerts[1].Voicing.B.ResidueNumber == 6 && Abs(TrsAr[0].B.Number) == 1) tr += 35;
+                        if (LVerts[1].Voicing.S.ResidueNumber == 6 && Abs(TrsAr[0].S.Number) == 0) tr -= 6;
+                        if (LVerts[1].Voicing.A.ResidueNumber == 6 && Abs(TrsAr[0].A.Number) == 0) tr -= 6;
+                        if (LVerts[1].Voicing.T.ResidueNumber == 6 && Abs(TrsAr[0].T.Number) == 0) tr -= 6;
+                        if (LVerts[1].Voicing.B.ResidueNumber == 6 && Abs(TrsAr[0].B.Number) == 0) tr -= 6;
+
+                        //Movement after dim 7
+                        if (Chords[0].Variety.PQ7 == -2 &&
+                            (Pitches[0].B) > (Pitches[1].B)) tr += 13;
+                        if (Chords[0].Variety.PQ7 == -2 &&
+                            (Pitches[0].S) < (Pitches[1].S)) tr += 13;
+
+                        //Chord transition
+                        if (Chords[1].Root == Chords[0].Root && Pitches[0].B == Pitches[1].B) tr += 65;
+                        switch ((Chords[1].Root - Chords[0].Root).ResidueNumber)
+                        {
+                            case 3: tr += -10; break;
+                            case 5: tr += -5; break;
+                            case 4: tr += 0; break;
+                            case 1: tr += 5; break;
+                            case 6: tr += 20; break;
+                            case 2: tr += 25; break;
+                            case 0: tr += 32; break;
+                        }
+
+                        //Paralells
+                        for (int i1 = 0; i1 < 4; i1++)
+                        {
+                            for (int i2 = i1 + 1; i2 < 4; i2++)
+                            {
+                                if (TrsMxR[0][i1] == TrsMxR[0][i2] && TrsMxR[0][i1] != new IntervalS())
+                                {
+                                    if (
+                                        PitchMxR[0][i1] - PitchMxR[0][i2] == new IntervalS(4, 7) ||
+                                        PitchMxR[0][i1] - PitchMxR[0][i2] == new IntervalS())
+                                    {
+                                        tr += 50;
+                                    }
+                                }
+                            }
+                        }
+
+                        //Overlapping
+                        for (int i1 = 0; i1 < 3; i1++)
+                        {
+                            int i2 = i1 + 1;
+
+                            if (PitchMx[0][i1] < PitchMx[1][i2])
+                            {
+                                tr += 45;
+                            }
+                            if (PitchMx[1][i1] < PitchMx[0][i2])
+                            {
+                                tr += 45;
+                            }
+                        }
+
+                    }
+
+
+                    if (Length == 3) // Length 3
+                    {
+
+                        //Chord transition 1 to 3
+                        if (Chords[2].Root == Chords[0].Root && Pitches[0].B == Pitches[2].B) tr += 40;
+
+                        //Bass jumping
+                        tr += 2.5 * Max(0, TrsAr[0].B.Semis - 2) * Max(0, TrsAr[1].B.Semis - 2);
+
+                    }
+
+
+                    return tr;
+                }
+            }
+
 
             public override bool Equals(object obj) => Equals(obj as Phrase);
 
