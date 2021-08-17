a5a2fe751df46d47d3e71844320a842abe3b9cd3
Giles Shaw <gilesgshaw@gmail.com>
2021-08-17 14:24:58 +0100

    add - 'Unit' class

---
+++ mus/frag/Unit.cs
@@ -0,0 +1,25 @@
+﻿namespace mus.Chorale
+{
+
+    // immutable, provided 'VoicingC' is
+    // currently vunerable to invalid inputs
+    // represents a chord voicing relative to an unknown 'note'
+    // by convention pitch = ('note' +s Root) +c Voicing
+    // so this does NOT narrow to Quad<IntervalC>
+    public class Unit : Quad<IntervalS>
+    {
+        public IntervalS ToRoot { get; }
+        public VoicingC Voicing { get; }
+
+        public Unit(IntervalS toRoot, VoicingC voicing) : base(
+            toRoot + voicing.S.Residue,
+            toRoot + voicing.A.Residue,
+            toRoot + voicing.T.Residue,
+            toRoot + voicing.B.Residue)
+        {
+            ToRoot = toRoot;
+            Voicing = voicing;
+        }
+    }
+
+}
